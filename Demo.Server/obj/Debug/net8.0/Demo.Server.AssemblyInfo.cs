e7f68f716bd4dc8f6003eda7721b561f7804a5bf
Sophie Williams <yokobe0012@gmail.com>
2024-08-27 01:50:40 +0800

    f

--- Demo.Server/obj/Debug/net8.0/Demo.Server.AssemblyInfo.cs
+++ Demo.Server/obj/Debug/net8.0/Demo.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fbca4d9e7c3013a9cd8d16c9210246136f850f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
